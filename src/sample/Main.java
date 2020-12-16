7ccb6f9c1fe181d2bad35a836acd14fc50054357
Andre <andre.agrippa@dcmail.ca>
2020-12-16 13:34:32 -0500

    Final changes, added image

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,23 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
 
 public class Main extends Application {
 
@@ -36,6 +43,12 @@ public class Main extends Application {
         Font font = Font.font("Consolas", FontWeight.BOLD, 20);
         clickMeButton.setFont(font);
 
+        FileInputStream imagePath = new FileInputStream("src/sample/asta.PNG");
+        Image asta = new Image(imagePath);
+        ImageView astaImageView = new ImageView(asta);
+        astaImageView.setFitWidth(200);
+        astaImageView.setPreserveRatio(true);
+
         // Step 1.2 - For button controls (or other event-type controls) - set the event
         clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -47,17 +60,20 @@ public class Main extends Application {
         });
 
         // Step 2 - create a container
-        HBox hbox = new HBox(10, helloLabel);
-        // HBox hbox = new HBox(helloLabel, clickMeBUtton);
+        HBox hbox = new HBox(10, helloLabel, goodByeLabel, astaImageView);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.setStyle("-fx-border-color: black");
+        hbox.setStyle("-fx-alignment: center");
 
-        //VBox vbox = new VBox(helloLabel, clickMeBUtton);
 
         GridPane gridPane = new GridPane();
 
         gridPane.add(helloLabel, 1, 0);
         gridPane.add(goodByeLabel, 1, 1);
         gridPane.add(clickMeButton, 2, 2);
+        gridPane.add(astaImageView, 3,3);
 
+        gridPane.setStyle("-fx-border-color: black;");
         // Step 3 - add layout container to scene
         Scene scene = new Scene(gridPane, WIDTH, HEIGHT);
 
